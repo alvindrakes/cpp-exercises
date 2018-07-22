fe3512f93165c7926b7128720091bef5fcaeee68
alvindrakes <alvinlohjinxian@gmail.com>
2018-07-22 20:44:34 +0800

    Added more listings

--- Lesson-2/exercise-2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-// c++ program must always return a value, therefore main must be int 
-int main() 
-{
-    std::cout << "Is there a bug here?" << std::endl; 
-    return 0;   
-}
